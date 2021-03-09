f18fe09c46fa7adb8ab1aad06de2daa716dc2260
Deyna Contreras <deyna.alexac@gmail.com>
2021-03-09 14:02:18 -0300

    Desafio culminado

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -16,17 +16,13 @@ class WinesController < ApplicationController
               strain_percents.delete("") #Limpia el array
 
               if @wine.save
-
                      strain_ids.length.times do |i| #Recorrro n veces el array de ids que tiene el mismo largo que el de porcentajes
-                            
                             WineStrain.create( #Creo una relacion entre los vinos y cepas
                                    wine_id: @wine.id, #Asigno el id del vino a la relacion
                                    strain_id: strain_ids[i], #asigno el id de la cepa a la relacion
                                    percent: strain_percents[i] #Asigno el porcentaje de la cepa a la relacion
                             )
-
                      end
-
                      redirect_to root_path, notice: "Vino creado éxitosamente"
               else
                      redirect_to root_path, alert: "Ocurrió un error creando el vino"
